80a306180fbf69581121b235a1c50519ee7df611
ThbltFr <77073688+ThibaultFr59@users.noreply.github.com>
2026-01-27 21:50:24 +0100

    feat: add search form functionality and event listener for user profile retrieval

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
 import "./style.css";
+
+// Select HTML elements
+const searchForm = document.getElementById("search-form");
+const searchInput = document.getElementById("text-field");
+const mainContainer = document.getElementById("profile-container");
+
+// Add Event Listener
+
+searchForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  getUserProfile(searchInput.value);
+});
+
+async function getUserProfile(username) {
+  console.log(username);
+  const url = `https://api.github.com/users/${username}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+}
